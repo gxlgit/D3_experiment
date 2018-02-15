93ca47e246d7ccbf77812456403ce3a8e5a105c2
Gwen Latasa <gxlatasa@gmail.com>
2018-02-15 13:37:40 -0500

    + y-axis label

--- script.js
+++ script.js
@@ -79,3 +79,57 @@ function type(d) {
   d.value = +d.value; // coerce to number
   return d;
 }
+
+
+//SVG bar graph vertical
+
+// set the dimensions and margins of the graph
+var margin = {top: 20, right: 20, bottom: 50, left: 70},
+    width = 960 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
+
+let scaleVert = d3.scaleLinear()
+// for vertical bars the range has to be reversed
+    .range([height, 0]);
+
+// var xAxis = d3.svg.axisBottom()
+
+var yAxis = d3.axisLeft(scaleVert)
+
+var chartVert = d3.select(".bar-graph-vertical")
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", height + margin.top + margin.bottom)
+
+    // text label for the y axis
+chartVert.append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 0 )
+      .attr("x",0 - (height / 2))
+      .attr("dy", "1em")
+      .style("text-anchor", "middle")
+      .text("Value");
+
+d3.tsv("data.tsv", type, function(error, data) {
+  scaleVert.domain([0, d3.max(data, function(d) { return d.value; })]);
+
+  //here we are evenly dividing the area depending on the # of bars we will have
+  var barWidth = width / data.length;
+
+  var bar = chartVert.selectAll("g")
+      .data(data)
+    .enter().append("g")
+      .attr("transform", function(d, i) { return "translate(" + i * barWidth + ",0)"; })
+
+    
+// y is how far from the top, the x is how far from the left.
+  bar.append("rect")
+      .attr("y", function(d) { return scaleVert(d.value); })
+      .attr("height", function(d) { return height - scaleVert(d.value); })
+      .attr("width", barWidth - 1);
+
+  bar.append("text")
+      .attr("x", barWidth / 2)
+      .attr("y", function(d) { return scaleVert(d.value) + 3; })
+      .attr("dy", ".75em")
+      .text(function(d) { return `${d.value} ${d.name}`; });
+});
